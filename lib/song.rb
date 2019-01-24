bac91afbaf739b0cba3db9f92df90e5031fa14dc
Michael J. Schiumo <mschiumo18@gmail.com>
2019-01-24 20:32:08 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,28 @@
+class Song 
+  
+  attr_accessor :name, :artist, :genre
+  
+   @@count = 0
+   
+   @@genres = []
+   
+  def intialize
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+  end  
+  
+  def self.count
+    @@count
+  end  
+  
+  def self.genres 
+    @genre = [] unless @genre
+    @genre << genre
+  end  
+  
+end  
+
+
+
