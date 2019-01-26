d44b98884ba3002666426bc52dd3a921b3cb4f6e
Michael J. Schiumo <mschiumo18@gmail.com>
2019-01-26 01:26:28 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -41,13 +41,13 @@ class Song
   
   def self.genre_count
     genre_count = {}
-    genre_count.each do |genre|
+    @@genres.each do |genre|
       if genre_count[genre]
         genre_count[genre] += 1 
-      else 
-        genre_count[genre] = 1 
-      end 
+      else
+        genre_count[genre] = 1
+      end
     end
     genre_count
-  end  
-end  
+  end
+end
